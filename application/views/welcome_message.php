2532716f964bf58c9ff9ed1729b6b1ccadfaef8f
idelfrides <idelfridesjorge@alu.ufc.br>
2018-05-27 12:49:57 -0300

    creating a new folder into static one

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -85,6 +85,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+    <div>
+        <?php
+            show_404();
+        ?>
+    </div>
 </div>
 
 </body>
