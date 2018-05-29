06c30c06587ea5daa47a5f98c6a6bb4ef3e09c41
IDELFRIDES JORGE <idelfridesjorge@alu.ufc.br>
2018-05-29 15:45:47 -0300

    Update Membro.php

--- application/controllers/Membro.php
+++ application/controllers/Membro.php
@@ -22,7 +22,7 @@ class Membro extends CI_Controller
 
     public function contactMember()
     {
-        $numMember = 4;   // numero de membros
+        $numMember = 10;/ numero de membros
 
         $dadosForm['nome'] = $this->input->post('InputName');
         $dadosForm['email'] = $this->input->post('InputEmail');
